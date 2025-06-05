cd45ab097a9e156dc9fc224c368404808c2737c0
itzSaan <sangramm7@gmail.com>
2025-06-05 14:15:09 +0530

    Finished Gatsby Tutorial Part 6

---
+++ src/pages/blog/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { graphql, Link } from "gatsby";
+import Layout from "../../components/layout";
+import Seo from "../../components/seo";
+
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.name}>
+          <h2>
+            <Link to={`/blog/${node.frontmatter.slug}`}>
+              {node.frontmatter.title}
+            </Link>
+          </h2>
+          <p>Posted: {node.frontmatter.date}</p>
+        </article>
+      ))}
+    </Layout>
+  );
+};
+
+// fetch the blogs
+export const query = graphql`
+  query {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+          slug
+        }
+        id
+      }
+    }
+  }
+`;
+
+export const Head = () => <Seo pageTitle="My Blog Posts" />;
+
+export default BlogPage;
